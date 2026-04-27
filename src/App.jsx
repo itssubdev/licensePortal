10cf51d6074b272039247058bc2750c37aeb6a4a
itssubdev <tiwarisubash730@gmail.com>
2026-04-27 18:59:49 +0545

    fix analytics setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import LicenseChecker from './components/LicenseChecker'
 import './App.css'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="container">
       <LicenseChecker />
+       <Analytics />
     </div>
   )
 }
